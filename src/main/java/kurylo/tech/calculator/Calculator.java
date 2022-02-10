3872bfdcb4ef7ae67d11539400c328fc3e992e1d
Konrad Kuryło <konrad.kurylo@orange.com>
2022-02-10 22:20:22 +0100

    add simple calculator method with first requirement - return 0 for empty string

---
+++ src/main/java/kurylo/tech/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package kurylo.tech.calculator;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.NONE)
+public class Calculator {
+    public static int add(String numbers){
+        return 0;
+    }
+}
---
+++ src/test/java/kurylo/tech/calculator/CalculatorTest.java
@@ -0,0 +1,28 @@
+package kurylo.tech.calculator;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class CalculatorTest {
+
+    @ParameterizedTest
+    @EnumSource(CalculatorScenario.class)
+    public void parameterizedTest(CalculatorScenario calculatorScenario){
+        Assertions
+                .assertThat(Calculator.add(calculatorScenario.getInput()))
+                .isEqualTo(calculatorScenario.getResult());
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum CalculatorScenario {
+        EMPTY_STRING("",0);
+
+        private final String input;
+        private final int result;
+    }
+
+}
