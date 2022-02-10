fa3e31d6d222ff95cd4e58653560ed6df5c814d3
Konrad Kuryło <konrad.kurylo@orange.com>
2022-02-11 00:20:24 +0100

    move responsibility to prepare delimiter and inputToProcess to preprocessor class

--- src/main/java/kurylo/tech/calculator/Calculator.java
+++ src/main/java/kurylo/tech/calculator/Calculator.java
@@ -1,26 +1,25 @@
 package kurylo.tech.calculator;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.stream.Stream;
 
 @NoArgsConstructor(access = AccessLevel.NONE)
 public class Calculator {
-    private static final String DEFAULT_DELIMITER = ",";
 
     public static int add(String numbers){
         if(numbers.isEmpty()){
             return 0;
         } else {
-            return nonEmptyStringCalculation(numbers);
+            Preprocessor preprocessor = Preprocessor.of(numbers);
+            return nonEmptyStringCalculation(preprocessor.getInputToProcess(), preprocessor.getDelimiter());
         }
     }
 
-    private static Integer nonEmptyStringCalculation(String numbers) {
-        boolean customDelimiterPresence = numbers.startsWith("//");
-        String delimiter = customDelimiterPresence ? numbers.substring(2,3) : DEFAULT_DELIMITER;
-        String inputToProcess = customDelimiterPresence ? numbers.substring(3) : numbers;
+    private static Integer nonEmptyStringCalculation(String inputToProcess, String delimiter) {
         if( inputToProcess.contains(delimiter + "\n") || inputToProcess.contains("\n" + delimiter) ) {
             throw new IllegalArgumentException("Delimiter and 'next line sign' are next to each other. Wrong input");
         } else if (inputToProcess.replaceAll("["+ delimiter + "\n]", "").matches("\\D*")){
@@ -31,5 +30,24 @@ public class Calculator {
                     .map(Integer::parseInt)
                     .reduce(0, Integer::sum);
         }
+    }
+
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    static class Preprocessor {
+        private static final String DEFAULT_DELIMITER = ",";
+        private final String delimiter;
+        private final String inputToProcess;
+
+        static Preprocessor of(String input){
+            boolean customDelimiterPresence = input.startsWith("//");
+            return new Preprocessor(
+                    customDelimiterPresence ? input.substring(2,3) : DEFAULT_DELIMITER,
+                    customDelimiterPresence ? input.substring(3) : input
+            );
+        }
+
+
+
     }
 }
