d62bc59d67327cad34d83ce5e45c619eb202c586
Konrad Kuryło <konrad.kurylo@orange.com>
2022-02-10 23:21:34 +0100

    add implementation to handle two numbers

--- src/main/java/kurylo/tech/calculator/Calculator.java
+++ src/main/java/kurylo/tech/calculator/Calculator.java
@@ -3,13 +3,17 @@ package kurylo.tech.calculator;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.stream.Stream;
+
 @NoArgsConstructor(access = AccessLevel.NONE)
 public class Calculator {
     public static int add(String numbers){
         if(numbers.isEmpty()){
             return 0;
         } else {
-            return Integer.parseInt(numbers);
+            return Stream.of(numbers.split(","))
+                    .map(Integer::parseInt)
+                    .reduce(0, Integer::sum);
         }
     }
 }
